abe35f0733b05ff92321ff9019355b35b9b25611
Aleksandr Podporin <89032213436@mail.ru>
2022-07-17 21:51:18 +0300

    первая проба

---
+++ script.js
@@ -0,0 +1,29 @@
+let profileEditButton = document.querySelector('.profile__edit-button');
+let popup = document.querySelector('.popup');
+let popupInputName = document.querySelector('.popup__input_value_name');
+let profileTitle = document.querySelector('.profile__title');
+let popupInputHobby = document.querySelector('.popup__input_value_hobby');
+let profileAbout = document.querySelector('.profile__subtitle');
+let profileCloseButton = document.querySelector('.popup__close-button');
+let popupForm = document.querySelector('.popup__form');
+
+function popupOpen() {
+    popupInputName.value = profileTitle.textContent;
+    popupInputHobby.value = profileAbout.textContent;
+    popup.classList.add('popup_opened');
+}
+
+function popupClose() {
+    popup.classList.remove('popup_opened');
+}
+
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+    profileTitle.textContent = popupInputName.value;
+    profileAbout.textContent = popupInputHobby.value;
+    popupClose();
+}
+
+profileEditButton.addEventListener('click', popupOpen);
+popupForm.addEventListener('submit', formSubmitHandler);
+profileCloseButton.addEventListener('click', popupClose);
